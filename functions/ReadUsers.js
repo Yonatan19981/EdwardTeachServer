97f45b34586be128c8bfb13fd32b094727dce17f
Yonatan Martsiano <Yonatan888@gmail.com>
2023-01-26 21:35:54 +0200

    updates

--- functions/ReadUsers.js
+++ functions/ReadUsers.js
@@ -1,38 +1,4 @@
 
-// Our standard serverless handler function
-exports.handler = async event => {
-// Grab our credentials from a .env file or environment variables
-require('dotenv').config({ path: '../.env'});
-require('dotenv').config();
-// Replace the uri string with your connection string.
-const uri =process.env.MONGO_URL ;
-console.log(uri)
-
-var client = new MongoClient(uri);
-
-    try {
-        const database = client.db('EdwardTeach');
-        const userData = database.collection('UserData');
-        try {
-            //userData.insertOne( { item: "card", qty: 15 } );
-            const cursor = userData.find();
-            await cursor.forEach(console.log);
-            return cursor;
-         } catch (e) {
-            print (e);
-         };
-         
-         
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-    // Did it work?
-    console.log(data, error);
-    
-  }
-  
-
   const { MongoClient } = require("mongodb");
 
   exports.handler = async function (event, context) {
